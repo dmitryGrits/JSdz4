bbeac74578e75099696c65017fb94dd06f3f6f19
DGRITS <grizay@icloud.com>
2024-01-21 15:11:39 +0300

    2 task

--- 1.js
+++ 1.js
@@ -10,12 +10,12 @@
 10 – четное число
 */
 
-for (let i = 0; i < 11; i++) {
-    if (i === 0) {
-        console.log(`0 – это ноль`);
-    } else if (i % 2 === 0) {
-        console.log(`${i} – четное число`);
-    } else {
-        console.log(`${i} – нечетное число`);
-    }
-}
+// for (let i = 0; i < 11; i++) {
+//     if (i === 0) {
+//         console.log(`0 – это ноль`);
+//     } else if (i % 2 === 0) {
+//         console.log(`${i} – четное число`);
+//     } else {
+//         console.log(`${i} – нечетное число`);
+//     }
+// }
